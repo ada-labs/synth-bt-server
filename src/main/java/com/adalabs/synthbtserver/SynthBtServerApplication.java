87368c7925075d16404f2a986afa4222f4912e8b
Alex Guerra <alex@ada-labs.com>
2019-07-06 00:38:55 -0500

    Initial commit

---
+++ src/main/java/com/adalabs/synthbtserver/SynthBtServerApplication.java
@@ -0,0 +1,13 @@
+package com.adalabs.synthbtserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SynthBtServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SynthBtServerApplication.class, args);
+    }
+
+}
