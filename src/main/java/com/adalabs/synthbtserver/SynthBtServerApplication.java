43602753dd2c6c2713cf9d935376d6b5ac4e6f5c
Alex Guerra <alex@ada-labs.com>
2019-07-06 00:55:42 -0500

    Update main java file to be a controller and write hello world

--- src/main/java/com/adalabs/synthbtserver/SynthBtServerApplication.java
+++ src/main/java/com/adalabs/synthbtserver/SynthBtServerApplication.java
@@ -2,9 +2,19 @@ package com.adalabs.synthbtserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 @SpringBootApplication
 public class SynthBtServerApplication {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String hello() {
+        return "Hello, World!";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SynthBtServerApplication.class, args);
